1995d1c6443adadc114c0fdbb5e33882219965d2
Le Ngoc Trong <ntrong0603.dgt@gmail.com>
2019-04-14 20:04:53 +0700

    class

---
+++ Bai 12 - class/index.js
@@ -0,0 +1,12 @@
+//class
+class Mouse{
+    constructor(name){
+        this.name = name;
+    }
+
+    run(){
+        console.log(`${this.name} is running`);
+    }
+}
+const mickey = new Mouse("Mickey");
+mickey.run();
